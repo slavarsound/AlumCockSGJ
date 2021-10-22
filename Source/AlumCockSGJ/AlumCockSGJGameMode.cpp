620fc8d02e628e1fb0ab2bc90aa4805a9431f607
reddification <reddification@gmail.com>
2021-10-23 00:39:50 +0500

    test commit 2

--- Source/AlumCockSGJ/AlumCockSGJGameMode.cpp
+++ Source/AlumCockSGJ/AlumCockSGJGameMode.cpp
@@ -1,12 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "AlumCockSGJGameMode.h"
 #include "Characters/AlumCockSGJCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AAlumCockSGJGameMode::AAlumCockSGJGameMode()
 {
-	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
